5446b548d07c7ea378e27ff302c36227ff9c0d72
Vladik Leshko <leshkovladik@gmail.com>
2023-03-14 13:08:44 +0300

    slider work

--- js/slider.js
+++ js/slider.js
@@ -1,117 +1,70 @@
 var $swiperSelector = $('.swiper-container');
 let slidesList = document.querySelectorAll(".swiper-slide");
-let slidesContent = document.querySelectorAll(".slider_item_container_hover");
-let slidesTextContainer = document.querySelectorAll(".sliderTextContainer");
-let slidesText = document.querySelectorAll(".sliderText");
-let videoFrame = document.querySelectorAll(".videoFrame");
 let slidesCount = 2.5;
+let space = 20;
 
 window.onload = function() {
     renderSlider();
 }
 
 slidesList.forEach((elem) => {
-    elem.addEventListener("click", (event) => openSlide(event, elem));
+    elem.addEventListener("click", (event) => openSlide());
+    elem.addEventListener("mouseover", (event) => mouseOver(event, elem));
+    elem.addEventListener("mouseout", (event) => mouseOut(event, elem));
 });
 
-slidesList.forEach((elem) => {
-    elem.addEventListener("mouseover", (event) => hoverSlide(event, elem));
-});
-
-slidesList.forEach((elem) => {
-    elem.addEventListener("mouseout", (event) => unhoverSlide(event, elem));
-});
-
-const openSlide = (event, elem) => {
+const openSlide = () => {
     if(slidesCount === 2.5) {
         slidesCount = 1;
-        slidesList.forEach((elem) => {
-            elem.classList.add("clickedSlide");
-            elem.classList.toggle("swiper-slide-white");
-        });
-        slidesContent.forEach((elem) => {
-            elem.classList.add("slider_item_container_active");
-        });
-        slidesTextContainer.forEach((elem) => {
-            elem.classList.add("clickedSlideTextContainer");
-        });
-        slidesText.forEach((elem) => {
-            elem.classList.add("clickedSlideText");
-        });
-        videoFrame.forEach((elem) => {
-            elem.classList.add("videoFrameSmall");
-        });
         renderSlider();
     } else {
         slidesCount = 2.5;
-        slidesList.forEach((elem) => {
-            elem.classList.remove("clickedSlide");
-            elem.classList.toggle("swiper-slide-white");
-        });
-        slidesContent.forEach((elem) => {
-            elem.classList.remove("slider_item_container_active");
-        });
-        slidesTextContainer.forEach((elem) => {
-            elem.classList.remove("clickedSlideTextContainer");
-        });
-        slidesText.forEach((elem) => {
-            elem.classList.remove("clickedSlideText");
-        });
-        videoFrame.forEach((elem) => {
-            elem.classList.remove("videoFrameSmall");
-        });
         renderSlider();
     }
 };
 
-const hoverSlide = (event, elem) => {
-    slidesList.forEach((elem) => {
-        if(event.currentTarget != elem) {
-            elem.classList.add("scaled-slide");
-        } 
-    });
+const mouseOver = (event, elem) => {
+    if(slidesCount !== 1) {
+        slidesList.forEach((slide) => {
+            if(slide != elem) {
+                slide.classList.add("scaled-slide-min");
+                space = 50;
+            } else {
+                slide.classList.add("scaled-slide-max");
+            }
+        });
+    }
 };
 
-const unhoverSlide = (event, elem) => {
-    slidesList.forEach((elem) => {
-        if(event.currentTarget != elem) {
-            elem.classList.remove("scaled-slide");
-        }
-    });
+const mouseOut = (event, elem) => {
+    if(slidesCount !== 1) {
+        slidesList.forEach((slide) => {
+            if(slide != elem) {
+                slide.classList.remove("scaled-slide-min");
+                space = 20;
+            } else {
+                slide.classList.remove("scaled-slide-max");
+            }
+        });
+    }
 };
 
 function renderSlider() {
-    $swiperSelector.each(function(index) {
-    var $this = $(this);
-    $this.addClass('swiper-slider-' + index);
+    var $swiper = $(".swiper-container");
+    var $bottomSlide = null; 
+    var $bottomSlideContent = null; 
 
-    var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
-    var loop = $this.data('loop') ? $this.data('loop') : false;
-    var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : slidesCount;
-    var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : slidesCount;
-    var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 1;
-    var spaceBetween = $this.data('space-between') ? $this.data('space-between'): 20;
-
-    var swiper = new Swiper('.swiper-slider-' + index, {
-        direction: 'horizontal',
-        loop: true,
-        freeMode: freeMode,
-        spaceBetween: spaceBetween,
-        breakpoints: {
-        1920: {
-            slidesPerView: slidesDesktop
-        },
-        992: {
-            slidesPerView: slidesTablet
-        },
-        400: {
-            slidesPerView: slidesMobile
-        }
-        },
+    var mySwiper = new Swiper(".swiper-container", {
+        spaceBetween: space,
+        initialSlide: 1,
+        slidesPerView: slidesCount,
+        centeredSlides: true,
+        roundLengths: true,
+        loop: false,
+        loopAdditionalSlides: 30,
         navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-        },
-    });
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
     });
 }
